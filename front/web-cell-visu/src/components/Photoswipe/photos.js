a97829203de9f05e0e1079a146aaeac10956e660
charles-gabriel-cc <charles.gabriel.cc@gmail.com>
2021-10-04 14:33:57 -0300

    new examples

--- front/web-cell-visu/src/components/Photoswipe/photos.js
+++ front/web-cell-visu/src/components/Photoswipe/photos.js
@@ -1,6 +1,15 @@
 import example1 from '../../assets/example-image-1.png'
 import example2 from '../../assets/example-image-2.png'
 import example3 from '../../assets/example-image-3.png'
+import example4 from '../../assets/example-image-6.png'
+import example5 from '../../assets/example-image-7.jpg'
+import example6 from '../../assets/example-image-8.jpg'
+import example7 from '../../assets/example-image-9.jpg'
+import example8 from '../../assets/example-image-10.jpg'
+import example9 from '../../assets/example-image-11.jpg'
+import example10 from '../../assets/example-image-12.jpg'
+import example11 from '../../assets/example-image-13.gif'
+import example12 from '../../assets/example-image-14.gif'
 
 export const photos = [
     {
@@ -17,6 +26,51 @@ export const photos = [
       src: example3,
       width: 5,
       height: 5
+    },
+    {
+      src: example4,
+      width: 5,
+      height: 5
+    },
+    {
+      src: example5,
+      width: 5,
+      height: 4
+    },
+    {
+      src: example6,
+      width: 5,
+      height: 4
+    },
+    {
+      src: example7,
+      width: 5,
+      height: 4
+    },
+    {
+      src: example8,
+      width: 4,
+      height: 6
+    },
+    {
+      src: example9,
+      width: 5,
+      height: 4
+    },
+    {
+      src: example10,
+      width: 5,
+      height: 4
+    },
+    {
+      src: example11,
+      width: 5,
+      height: 4
+    },
+    {
+      src: example12,
+      width: 5,
+      height: 4
     }
   ];
   
